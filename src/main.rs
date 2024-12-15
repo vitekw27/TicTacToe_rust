c882951ac0f68548553638216e1e67fa2a369af8
= <=>
2024-12-15 19:16:04 +0000

    Fully working

--- src/main.rs
+++ src/main.rs
@@ -19,15 +19,18 @@ impl Game{
         }
     }
 
-    pub fn play(&mut self){
+    pub fn play(&mut self) {
         loop {
-            self.display_board();
-            if self.check_win() {
-                return;
-            }
-            self.switch_player(); 
+            self.display_board(); // Display the board
+            
+            self.take_turn(); // Let the current player take a turn
     
-            self.take_turn();
+            if self.check_win() { // Check if the current player has won               
+                self.display_board(); // Display final state of the board
+                break; // Exit the loop
+            }
+    
+            self.switch_player(); // Switch to the next player
         }
     }
     pub fn display_board(&self){
@@ -75,28 +78,50 @@ impl Game{
 
     }
     
-    pub fn check_win(&self) -> bool{
-
-        for row in 0..3  {
-           let row_values = &self.board[row];
-
-            if &row_values[0] == &self.current_player && &row_values[1] == &self.current_player && &row_values[2] == &self.current_player{
-                println!("Player {:?} has won!",&self.current_player);
-                return true;
-            }
-            return false;
-
-        }
-        for column in 0..3  {
-            if &self.board[0][column] == &self.current_player && &self.board[1][column] == &self.current_player && &self.board[2][column] == &self.current_player {
-                println!("Player {:?} has won!",&self.current_player);
+    pub fn check_win(&self) -> bool {
+        // Check rows
+        for row in 0..3 {
+            if self.board[row][0] == self.current_player
+                && self.board[row][1] == self.current_player
+                && self.board[row][2] == self.current_player
+            {
+                println!("Player {} has won!", self.current_player);
                 return true;
             }
         }
-
-        return false;
-        
+    
+        // Check columns
+        for column in 0..3 {
+            if self.board[0][column] == self.current_player
+                && self.board[1][column] == self.current_player
+                && self.board[2][column] == self.current_player
+            {
+                println!("Player {} has won!", self.current_player);
+                return true;
+            }
+        }
+    
+        // Check first diagonal
+        if self.board[0][0] == self.current_player
+            && self.board[1][1] == self.current_player
+            && self.board[2][2] == self.current_player
+        {
+            println!("Player {} has won!", self.current_player);
+            return true;
+        }
+    
+        // Check second diagonal
+        if self.board[0][2] == self.current_player
+            && self.board[1][1] == self.current_player
+            && self.board[2][0] == self.current_player
+        {
+            println!("Player {} has won!", self.current_player);
+            return true;
+        }
+    
+        false // No win condition met
     }
+    
 }
 
 
